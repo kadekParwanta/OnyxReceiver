8286eff6d93461b1e6e362cb2f123c78cd35c49e
kadekParwanta <kadek.parwanta@gmail.com>
2017-01-31 10:57:27 +0800

    add com-cordova-ble plugin

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "com-cordova-ble.ble",
+        "file": "plugins/com-cordova-ble/www/ble.js",
+        "pluginId": "com-cordova-ble",
+        "clobbers": [
+            "ble"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.2-dev"
+    "cordova-plugin-whitelist": "1.3.2-dev",
+    "com-cordova-ble": "2.0.1-dev"
 };
 // BOTTOM OF METADATA
 });
